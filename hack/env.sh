69c5fd4fac46b9073ecbec7d67805cc59e1d956f
sunxiaoning <1530358579@qq.com>
2024-10-19 20:55:08 +0800

    feat: update the `rpmsync` submodule.

--- hack/env.sh
+++ hack/env.sh
@@ -4,12 +4,12 @@ set -o pipefail
 
 PKG_PATH="hack/pack"
 
-PKG_VERSION="1.0.0"
+PKG_VERSION="1.0.1"
 PKG_NAME="rpmserver-${PKG_VERSION}.tar.gz"
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
 REL_NOTES="
-## Init Release
-- Released the \`rpmserver\` project.
+## Improvements
+- Updated the \`rpmsync\` submodule.
 "
