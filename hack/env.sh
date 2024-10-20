78703e6a7cd533d0ccdad59871436db10ae13841
sunxiaoning <1530358579@qq.com>
2024-10-20 15:04:13 +0800

    feat: update pkg version and release notes.

--- hack/env.sh
+++ hack/env.sh
@@ -4,12 +4,12 @@ set -o pipefail
 
 PKG_PATH="hack/pack"
 
-PKG_VERSION="1.0.2"
+PKG_VERSION="1.0.3"
 PKG_NAME="rpmserver-${PKG_VERSION}.tar.gz"
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
 REL_NOTES="
 ## Improvements
-- Fix the \`RENDER_SH_FILE\` variable.
+- Optimized the \`run-reposerver\` progress
 "
