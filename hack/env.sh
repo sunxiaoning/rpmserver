f544f1afab26f309cb5268e5178ea57df5f4d84a
sunxiaoning <1530358579@qq.com>
2024-11-12 14:40:00 +0800

    feat: update the `bashutils` submodule

--- hack/env.sh
+++ hack/env.sh
@@ -4,12 +4,12 @@ set -o pipefail
 
 PKG_PATH="hack/pack"
 
-PKG_VERSION="1.0.5"
+PKG_VERSION="1.0.6"
 PKG_NAME="rpmserver-${PKG_VERSION}.tar.gz"
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
 REL_NOTES="
 ## Improvements
-- Optimized the \`reload-or-restart-reposerver\` progress
+- Updated the bashutils submodule
 "
