7643207f9bdb67b216da3a9fc2006ecbad1a135a
sunxiaoning <1530358579@qq.com>
2024-11-23 14:59:19 +0800

    feat: update bashutils submodule

--- rpmserver.sh
+++ rpmserver.sh
@@ -68,12 +68,12 @@ main() {
 }
 
 terminate() {
-  echo "terminating..."
+  echo "[${SCRIPT_NAME}] Terminating..."
 }
 
 cleanup() {
   if [[ "${#TEMP_FILES[@]}" -gt 0 ]]; then
-    echo "Cleaning temp_files...."
+    echo "[${SCRIPT_NAME}] Cleaning temp_files...."
 
     for temp_file in "${TEMP_FILES[@]}"; do
       rm -f "${temp_file}" || true
